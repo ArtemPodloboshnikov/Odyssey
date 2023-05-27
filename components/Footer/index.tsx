c427e9861ce0036f287db089d59b8fef82ec2a49
ArtemPodloboshnikov <ArtemPS777@yandex.ru>
2023-05-27 22:33:39 +0300

    refactor(footer): Delete map

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,24 +1,13 @@
 "use client"
 
 import { PHONE_LINK } from "@/constants/links";
-import { Icon, LatLngExpression } from "leaflet";
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import FirstVisit from "../FirstVisit";
 import { usePathname } from 'next/navigation';
 
 const Footer: React.FC = () => {
-    const position: LatLngExpression = [59.94041749831786, 30.323925288359895];
-    const markerIcon: Icon = new Icon({iconUrl: "/images/logo.jpg", iconSize: [30, 30], iconAnchor: [0, 0], className: "rounded-full relative before:animate-pulse before:content-[''] before:absolute before:left-0 before:top-0 before:bg-[--red] before:w-[100%] before:h-[100%]"});
     const path = usePathname();
     return (
         <footer className={`flex flex-col items-center mt-10 py-5 gap-y-5 ${path === "/admin" ? "hidden" : null}`}>
-            <MapContainer center={position} zoom={23} className="w-full h-[400px]">
-                <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={position} icon={markerIcon}>
-                </Marker>
-            </MapContainer>
             <div className="grid grid-cols-3 justify-items-center gap-x-20 max-lg:text-xs max-lg:gap-x-5">
                 <span className="text-center">г.Санкт-Петербург,<br/>Большая Конюшенная ул., 5</span>
                 <span className="text-center">© 2023 | Odyssey<br/>Все права защищены</span>
